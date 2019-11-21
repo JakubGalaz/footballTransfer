0867e4d0cb3b185252f5998fb0287bfdb7af62a7
JakubGa;az <kubixes@interia.pl>
2019-11-21 23:42:14 +0100

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TransferServiceService } from './transfer-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'footballTransfers';
+  menuOption: 0;
+  results;
+
+
+  constructor(public http: HttpClient, public transferService: TransferServiceService) {
+
+
+
+    this.transferService.MyList =
+  // tslint:disable-next-line: no-shadowed-variable
+  this.http.get('../assets/transfers.json').subscribe(data => {
+    this.results = data;
+
+    console.log(this.results[1].Name);
+
+
+  });
+
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TransferServiceService } from './transfer-service.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [TransferServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
